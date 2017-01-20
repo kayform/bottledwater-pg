536dd53bbdf6710d54fb91f69674230242b812d9
k4m <k4m@klmatrix.com>
2017-01-20 12:53:22 +0900

    add scripts that create column map table

--- ext/bottledwater--0.1.sql
+++ ext/bottledwater--0.1.sql
@@ -24,3 +24,22 @@ CREATE OR REPLACE FUNCTION bottledwater_export(
         error_policy bottledwater_error_policy DEFAULT 'exit'
     ) RETURNS setof bytea
     AS 'bottledwater', 'bottledwater_export' LANGUAGE C VOLATILE STRICT;
+
+-- Create column mappng tables
+CREATE TABLE public.col_mapps
+(
+  reloid oid NOT NULL,
+  column_name character varying(100) NOT NULL,
+  colseq integer NOT NULL,
+  create_date timestamp without time zone NOT NULL DEFAULT now(),
+  create_user character varying(100) NOT NULL DEFAULT "current_user"(),
+  remark character varying(1000),
+  CONSTRAINT pk_col_mapps PRIMARY KEY (reloid, column_name)
+)
+
+COMMENT ON COLUMN col_mapps.reloid is '테이블 OID';
+COMMENT ON COLUMN col_mapps.column_name is '컬럼명';
+COMMENT ON COLUMN col_mapps.colseq is '컬럼순번';
+COMMENT ON COLUMN col_mapps.create_date is '등록일자';
+COMMENT ON COLUMN col_mapps.create_user is '등록사용자';
+COMMENT ON COLUMN col_mapps.remark is '비고';
