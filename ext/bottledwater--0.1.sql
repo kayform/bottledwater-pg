1255a0b0ed8df7d4cdc86aaa4173f866945a3b4d
k4m <k4m@klmatrix.com>
2017-03-10 17:51:15 +0900

    add history table

--- ext/bottledwater--0.1.sql
+++ ext/bottledwater--0.1.sql
@@ -44,3 +44,30 @@ COMMENT ON COLUMN col_mapps.colseq is '컬럼순번';
 COMMENT ON COLUMN col_mapps.create_date is '등록일자';
 COMMENT ON COLUMN col_mapps.create_user is '등록사용자';
 COMMENT ON COLUMN col_mapps.remark is '비고';
+
+-- Create column mappng history tables
+DROP TABLE IF EXISTS public.col_mapps_hist;
+CREATE TABLE public.col_mapps_hist
+(
+  reloid oid NOT NULL,
+  column_name character varying(100) NOT NULL,
+  colseq integer NOT NULL,
+  create_date timestamp without time zone NOT NULL DEFAULT now(),
+  create_user character varying(100) NOT NULL DEFAULT "current_user"(),
+  mod_date timestamp not null default now(),
+  mod_user varchar(100) not null default current_user,
+  mod_type varchar(100) not null,
+  remark character varying(1000),
+  CONSTRAINT pk_col_mapps PRIMARY KEY (reloid, column_name)
+);
+
+COMMENT ON COLUMN col_mapps_hist.reloid is '테이블 OID';
+COMMENT ON COLUMN col_mapps_hist.column_name is '컬럼명';
+COMMENT ON COLUMN col_mapps_hist.colseq is '컬럼순번';
+COMMENT ON COLUMN col_mapps_hist.create_date is '등록일자';
+COMMENT ON COLUMN col_mapps_hist.create_user is '등록사용자';
+COMMENT ON COLUMN col_mapps_hist.mod_date is '이력날짜';
+COMMENT ON COLUMN col_mapps_hist.mod_user is '이력수행유저';
+COMMENT ON COLUMN col_mapps_hist.mod_type is '이력타입';
+COMMENT ON COLUMN col_mapps_hist.remark is '비고';
+
